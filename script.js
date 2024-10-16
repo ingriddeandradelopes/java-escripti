2d7c5be3cae3f5a15ecccd84d5726de294e12700
ingriddeandradelopes <163137409+ingriddeandradelopes@users.noreply.github.com>
2024-10-16 08:10:32 -0300

    2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function mudarcor(){
     const titulo = document.getElementById('titulo');
-    titulo.style.color=titulo.style.color==='red'?'blue':'red'
+    titulo.style.color=titulo.style.color==='red'?'blue':'red';
 }
